f92ae6698d5d93bbc09d3ce571388dee3a9245af
Tai Doan <taidoan@live.co.uk>
2025-03-08 12:54:19 +0000

    integrated breadcrumbs to all hero types

--- src/blocks/Hero/Small/index.tsx
+++ src/blocks/Hero/Small/index.tsx
@@ -1,7 +1,29 @@
 import type { HeroBlockProps } from '@/payload-types';
+import type { Breadcrumbs as BreadcrumbsType } from '@/components/ui/Breadcrumbs';
+import { Breadcrumbs } from '@/components/ui/Breadcrumbs';
 
-export type SmallHeroProps = HeroBlockProps;
+export type SmallHeroProps = HeroBlockProps & {
+  breadcrumbsData?: BreadcrumbsType;
+};
 
-export const SmallHero = ({ image, richText }: SmallHeroProps) => {
-  return <div>Small Hero</div>;
+export const SmallHero = ({
+  image,
+  richText,
+  showBreadcrumb,
+  breadcrumbsData,
+  breadcrumbContainer,
+  breadcrumbBackground,
+}: SmallHeroProps) => {
+  return (
+    <div>
+      Small Hero{' '}
+      {showBreadcrumb && breadcrumbsData && breadcrumbsData.length > 0 && (
+        <Breadcrumbs
+          breadcrumbs={breadcrumbsData}
+          container={breadcrumbContainer || 'none'}
+          background={breadcrumbBackground || 'none'}
+        />
+      )}
+    </div>
+  );
 };
