376238a040c3fb7ed44e5480eb80bcc80a806213
Tai Doan <taidoan@live.co.uk>
2025-04-01 02:23:25 +0100

    test(filter): Updated filter tests to use mock categories

--- src/components/ui/Filter/filter.stories.tsx
+++ src/components/ui/Filter/filter.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Filter } from './index';
-import type { Category } from '@/payload-types';
+import { mockCategories } from '@/mocks/data/mockCategories';
 
 const meta: Meta<typeof Filter> = {
   title: 'UI/Filter',
@@ -29,31 +29,6 @@ export const Categories: Story = {
       all: <span>🏷️</span>,
       'web-design': <span>🎨</span>,
     },
-    categories: [
-      {
-        description:
-          'Creating visually appealing and functional designs for physical materials, such as brochures, posters, flyers, business cards, and packaging. I ensure your printed materials leave a lasting impression.',
-        slug: 'print',
-        slugLock: true,
-        title: 'Print',
-        id: '67cece0800d7591f31a00957',
-      },
-      {
-        description:
-          'Crafting visually compelling websites that are easy to navigate and responsive across all devices. I focus on layout, typography, and interactive elements to create websites that look great and work even better.',
-        slug: 'web-design',
-        slugLock: true,
-        parentCategory: {
-          description:
-            "Your online presence is everything, and I’m here to help you make it unforgettable. From sleek, responsive websites to engaging digital experiences, I design with purpose and creativity. Whether it's building a site from scratch or enhancing your existing one, I focus on user-friendly, beautiful designs that work seamlessly across devices. Let’s bring your digital vision to life and make sure it stands out in the crowded online world.",
-          slug: 'digital',
-          slugLock: true,
-          title: 'Digital',
-          id: '67ceccd600d7591f31a008bc',
-        } as Category,
-        title: 'Web Design',
-        id: '67cecdbc00d7591f31a0091b',
-      },
-    ],
+    categories: mockCategories,
   },
 };
--- src/components/ui/Filter/filter.test.tsx
+++ src/components/ui/Filter/filter.test.tsx
@@ -1,35 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Filter } from './index';
-import type { Category } from '@/payload-types';
-
-type CategoryWithoutCTA = Omit<Category, 'ctaLink'>;
-
-const mockCategories: CategoryWithoutCTA[] = [
-  {
-    description:
-      'Creating visually appealing and functional designs for physical materials, such as brochures, posters, flyers, business cards, and packaging. I ensure your printed materials leave a lasting impression.',
-    slug: 'print',
-    slugLock: true,
-    title: 'Print',
-    id: '67cece0800d7591f31a00957',
-  },
-  {
-    description:
-      'Crafting visually compelling websites that are easy to navigate and responsive across all devices. I focus on layout, typography, and interactive elements to create websites that look great and work even better.',
-    slug: 'web-design',
-    slugLock: true,
-    parentCategory: {
-      description:
-        "Your online presence is everything, and I'm here to help you make it unforgettable. From sleek, responsive websites to engaging digital experiences, I design with purpose and creativity. Whether it's building a site from scratch or enhancing your existing one, I focus on user-friendly, beautiful designs that work seamlessly across devices. Let's bring your digital vision to life and make sure it stands out in the crowded online world.",
-      slug: 'digital',
-      slugLock: true,
-      title: 'Digital',
-      id: '67ceccd600d7591f31a008bc',
-    } as Category,
-    title: 'Web Design',
-    id: '67cecdbc00d7591f31a0091b',
-  },
-];
+import { mockCategories } from '@/mocks/data/mockCategories';
 
 const mockOnSelectCategory = vi.fn();
 
@@ -75,10 +46,11 @@ describe('<Filter>', () => {
       />,
     );
 
-    const printCategory = screen.getByText('Print');
-    fireEvent.click(printCategory);
-
-    expect(mockOnSelectCategory).toHaveBeenCalledWith('67cece0800d7591f31a00957');
+    mockCategories.forEach((category) => {
+      const categoryButton = screen.getByText(category.title);
+      fireEvent.click(categoryButton);
+      expect(mockOnSelectCategory).toHaveBeenCalledWith(category.id);
+    });
   });
 
   it('should render an all button if option is passed as prop', () => {
