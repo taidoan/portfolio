c945c60a2e3695d6ca0fb7089dd5ec5b410bf3cf
Tai Doan <taidoan@live.co.uk>
2025-04-14 23:09:31 +0100

    refactor(richtext): Seperated blocks to different file

--- src/components/ui/RichText/converters/heading.tsx
+++ src/components/ui/RichText/converters/heading.tsx
@@ -1,48 +1,13 @@
 import { JSXConvertersFunction, LinkJSXConverter } from '@payloadcms/richtext-lexical/react';
-import { LinksRichtextBlock } from '@/payload/blocks/LinkRichtext';
-import { LinksGroupRichtextBlock } from '@/payload/blocks/LinkRichtext/Group';
 import { Fragment } from 'react';
 import { internalDocToHref } from './../utils';
-import { SerializedHeadingNode, SerializedBlockNode } from '@payloadcms/richtext-lexical';
-import { DividerBlock } from '@/payload/blocks/Divider';
-import { MediaRichTextBlock } from '@/payload/blocks/MediaRichtext';
-import { ContactMethodsBlock } from '@/payload/blocks/ContactMethods';
-import type {
-  LinksBlockRichtextProps,
-  LinksGroupRichtextProps,
-  MediaRichtextBlockProps,
-  ContactMethodsBlockProps,
-} from '@/payload-types';
+import { SerializedHeadingNode } from '@payloadcms/richtext-lexical';
+import { blocks } from './../blocks';
 
 export const headingConverter: JSXConvertersFunction = ({ defaultConverters }) => ({
   ...defaultConverters,
   ...LinkJSXConverter({ internalDocToHref }),
-  blocks: {
-    divider: ({ node }: { node: SerializedBlockNode<{ blockType: 'divider' }> }) => {
-      if (node.fields.blockType !== 'divider') return null;
-      return <DividerBlock {...node.fields} />;
-    },
-    ['links-richtext']: ({ node }: { node: SerializedBlockNode<LinksBlockRichtextProps> }) => {
-      if (node.fields.blockType !== 'links-richtext') return null;
-      return <LinksRichtextBlock {...node.fields} />;
-    },
-    ['links-group-richtext']: ({
-      node,
-    }: {
-      node: SerializedBlockNode<LinksGroupRichtextProps>;
-    }) => {
-      if (node.fields.blockType !== 'links-group-richtext') return null;
-      return <LinksGroupRichtextBlock {...node.fields} />;
-    },
-    mediaRichtextBlock: ({ node }: { node: SerializedBlockNode<MediaRichtextBlockProps> }) => {
-      if (node.fields.blockType !== 'mediaRichtextBlock') return null;
-      return <MediaRichTextBlock {...node.fields} />;
-    },
-    contactMethodsBlock: ({ node }: { node: SerializedBlockNode<ContactMethodsBlockProps> }) => {
-      if (node.fields.blockType !== 'contactMethodsBlock') return null;
-      return <ContactMethodsBlock {...node.fields} />;
-    },
-  },
+  blocks,
   heading: (args) => {
     const { node, nodesToJSX } = args;
     const typedNode = node as SerializedHeadingNode;
