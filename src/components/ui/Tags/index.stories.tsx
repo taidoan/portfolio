1eb70d0e42a63f766f992c401029114aa9a87234
Tai Doan <taidoan@live.co.uk>
2025-05-23 11:00:56 +0100

    feat(tags): Added tag count to tags.

---
+++ src/components/ui/Tags/index.stories.tsx
@@ -0,0 +1,113 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Tag, TagsContainer } from './index';
+import { mockTags } from '@/mocks/data/mockTags';
+
+const meta: Meta<typeof Tag> = {
+  title: 'UI/Tags',
+  component: Tag,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          maxWidth: '800px',
+          margin: '0 auto',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    href: undefined,
+    variant: 'default',
+    color: 'default',
+  },
+  argTypes: {
+    href: {
+      control: 'text',
+      description: 'Add a link to the tag',
+      default: null,
+    },
+    className: {
+      description: 'Add a custom class to the tag',
+    },
+    variant: {
+      control: 'select',
+      description: 'The variant of the tag',
+      default: 'default',
+      options: ['default', 'border'],
+    },
+    color: {
+      control: 'select',
+      description: 'The color of the tag',
+      default: 'default',
+      options: ['default', 'light-grey', 'slate', 'dark-grey'],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Tag>;
+
+export const Default: Story = {
+  render: (args) => {
+    const tags = mockTags.map((tag) => (
+      <Tag key={tag.id} {...args}>
+        {tag.name}
+      </Tag>
+    ));
+
+    return <TagsContainer>{tags}</TagsContainer>;
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+  render: (args) => {
+    const tags = mockTags.map((tag) => (
+      <Tag key={tag.id} {...args}>
+        {tag.name}
+      </Tag>
+    ));
+
+    return <TagsContainer>{tags}</TagsContainer>;
+  },
+};
+
+export const Bordered: Story = {
+  args: {
+    variant: 'border',
+  },
+  render: (args) => {
+    const tags = mockTags.map((tag) => (
+      <Tag key={tag.id} {...args}>
+        {tag.name}
+      </Tag>
+    ));
+
+    return <TagsContainer>{tags}</TagsContainer>;
+  },
+};
+
+export const WithCount: Story = {
+  args: {
+    showCount: true,
+    count: 3,
+  },
+  render: (args) => {
+    const tags = mockTags.map((tag) => (
+      <Tag key={tag.id} {...args}>
+        {tag.name}
+      </Tag>
+    ));
+
+    return <TagsContainer>{tags}</TagsContainer>;
+  },
+};
--- src/components/ui/Tags/index.tsx
+++ src/components/ui/Tags/index.tsx
@@ -4,6 +4,10 @@ import clsx from 'clsx';
 import style from './style.module.scss';
 import * as React from 'react';
 
+export const TagContainer = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={clsx(className, style.tags__container)} {...props} />
+);
+
 export const TagsContainer = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div className={clsx(className, style.tags__container)} {...props} />
 );
@@ -12,15 +16,40 @@ const TagSpan = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>)
   <span className={clsx(className, style.tag)} {...props} />
 );
 
-const TagLink = ({ className, ...props }: { className?: string } & LinkProps) => (
-  <Link {...props} className={clsx(className, style.tag)} />
+type TagLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  LinkProps & {
+    className?: string;
+    children: React.ReactNode;
+  };
+
+const TagLink = ({ href, className, children, ...props }: TagLinkProps) => (
+  <Link href={href} className={clsx(style.tag, className)} {...props}>
+    {children}
+  </Link>
 );
+
+const TagCount = ({
+  count,
+  color = 'accent',
+}: {
+  count: number;
+  color?: 'primary' | 'secondary' | 'accent' | 'green' | 'red' | 'orange';
+}) => {
+  const classes = clsx(style['tag-count'], {
+    [style[`tag-count--clr-${color}`]]: color,
+  });
+
+  return <span className={classes}>{count}</span>;
+};
 
 export type TagProps = {
   className?: string;
   onClick?: React.MouseEventHandler<HTMLSpanElement | HTMLAnchorElement>;
   variant: 'default' | 'border';
   color: 'default' | 'light-grey' | 'slate' | 'dark-grey';
+  size?: 'default' | 'large';
+  showCount?: boolean;
+  count?: number;
 } & (
   | { href?: never; children: React.ReactNode | string }
   | ({ href: string; children: React.ReactNode | string } & LinkProps)
@@ -28,20 +57,36 @@ export type TagProps = {
 
 export const Tag = ({
   className,
+  children,
   href,
   onClick,
   variant = 'default',
   color = 'default',
+  size = 'default',
+  showCount = false,
+  count,
   ...props
 }: TagProps) => {
   const classes = clsx(className, {
     [style['tag-variant--bordered']]: variant === 'border',
+    [style[`tag-size--${size}`]]: size !== 'default',
     [style[`tag-clr--${color}`]]: color !== 'default',
+    [style['tag-clr--default']]: color === 'default',
   });
 
   if (href) {
-    return <TagLink href={href} onClick={onClick} className={classes} {...props} />;
+    return (
+      <TagLink href={href} onClick={onClick} className={classes} {...props}>
+        {children}
+        {showCount && count && count > 1 && <TagCount count={count} />}
+      </TagLink>
+    );
   }
 
-  return <TagSpan onClick={onClick} className={classes} {...props} />;
+  return (
+    <TagSpan onClick={onClick} className={classes} {...props}>
+      {children}
+      {showCount && count && count > 1 && <TagCount count={count} />}
+    </TagSpan>
+  );
 };
