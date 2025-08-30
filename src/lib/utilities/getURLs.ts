bf6de446957041db41f08b45f24ae29d27773583
Tai Doan <taidoan@live.co.uk>
2025-08-30 12:35:02 +0100

    fix(getURLs): Used const instead of let.

--- src/lib/utilities/getURLs.ts
+++ src/lib/utilities/getURLs.ts
@@ -1,7 +1,7 @@
 import canUseDOM from './canUseDOM';
 
 export const getServerSideURL = () => {
-  let url = process.env.NEXT_PUBLIC_SERVER_URL || '';
+  const url = process.env.NEXT_PUBLIC_SERVER_URL || '';
 
   return url;
 };
