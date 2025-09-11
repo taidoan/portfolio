cdca0d2b65e532db568518102bf5b8f9bc21cae2
Tai Doan <taidoan@live.co.uk>
2025-09-11 19:51:51 +0100

    feat(tooltip): Created tooltip component.

---
+++ src/components/ui/Tooltip/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useContext, createContext } from 'react';
+import clsx from 'clsx';
+import style from './style.module.scss';
+
+const TooltipContext = createContext<{
+  color?: 'dark' | 'light';
+  open: boolean;
+  setOpen: (open: boolean) => void;
+} | null>(null);
+
+export const Tooltip = ({
+  children,
+  defaultOpen = false,
+  color = 'dark',
+}: {
+  children: React.ReactNode;
+  defaultOpen?: boolean;
+  color?: 'dark' | 'light';
+}) => {
+  const [open, setOpen] = useState(defaultOpen);
+
+  return (
+    <TooltipContext.Provider value={{ open, setOpen, color }}>
+      <span className={clsx(style['tooltip__container'])}>{children}</span>
+    </TooltipContext.Provider>
+  );
+};
+
+Tooltip.Trigger = ({ children }: { children: React.ReactNode }) => {
+  const ctx = useContext(TooltipContext);
+  if (!ctx) throw new Error('Tooltip.Trigger must be used within a Tooltip');
+
+  return (
+    <span
+      onMouseEnter={() => ctx.setOpen(true)}
+      onMouseLeave={() => ctx.setOpen(false)}
+      onFocus={() => ctx.setOpen(true)}
+      onBlur={() => ctx.setOpen(false)}
+      className={clsx(style['tooltip__trigger'])}
+    >
+      {children}
+    </span>
+  );
+};
+
+Tooltip.Content = ({ children }: { children: React.ReactNode }) => {
+  const ctx = useContext(TooltipContext);
+  if (!ctx) throw new Error('Tooltip.Content must be used inside <Tooltip>');
+
+  return ctx.open ? (
+    <span className={clsx(style.tooltip, style[`tooltip--${ctx.color}`])}>{children}</span>
+  ) : null;
+};
